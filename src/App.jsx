5f74f34dde2ab40577fec66af74b731e65e5410a
n20021 <n20021@std.it-college.ac.jp>
2021-02-13 02:02:47 +0000

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import home from './components/home'
+import NasaPhoto from './components/NasaPhoto'
+import './App.css'
+import { BrowserRouter, Route } from 'react-router-dom'
+export default function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <div className='app'>
+        <Route component={home} path='/' exact />
+        <Route component={NasaPhoto} path='/nasaphoto' />
+      </div>
+    </BrowserRouter>
+  )
 }
-
-export default App;
---
+++ src/components/NasaPhoto.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react'
+
+export default function NasaPhoto () {
+  const [photoData, setPhotoData] = useState(null)
+
+  useEffect(() => {
+    fetchPhoto()
+
+    async function fetchPhoto () {
+      const res = await fetch(
+        'https://api.nasa.gov/planetary/apod?api_key=Xipplxu2U6SLOxLIBlPFNIcLCNaUM4WcvtcTpyLD'
+      )
+      const data = await res.json()
+      setPhotoData(data)
+    }
+  }, [])
+
+  if (!photoData) return <div />
+
+  return (
+    <div className='nasa-photo'>
+      <img src={photoData.url} alt={photoData.title} className='photo' />
+      <div>
+        <h1>{photoData.tile}</h1>
+        <p className='date'>{photoData.date}</p>
+        <p className='explanation'>{photoData.explanation}</p>
+      </div>
+    </div>
+  )
+}
