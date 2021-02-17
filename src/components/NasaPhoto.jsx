73787cc2fa1d4fe83001ec791c773dc87dd10102
n20021 <n20021@std.it-college.ac.jp>
2021-02-17 04:30:48 +0000

    fix

--- src/components/NasaPhoto.jsx
+++ src/components/NasaPhoto.jsx
@@ -1,14 +1,12 @@
 import React, { useState, useEffect } from 'react'
-
 export default function NasaPhoto () {
   const [photoData, setPhotoData] = useState(null)
-
   useEffect(() => {
     fetchPhoto()
 
     async function fetchPhoto () {
       const res = await fetch(
-        'https://api.nasa.gov/planetary/apod?api_key=Xipplxu2U6SLOxLIBlPFNIcLCNaUM4WcvtcTpyLD'
+        `https://api.nasa.gov/planetary/apod?api_key=Xipplxu2U6SLOxLIBlPFNIcLCNaUM4WcvtcTpyLD`
       )
       const data = await res.json()
       setPhotoData(data)
